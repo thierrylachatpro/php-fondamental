bb6de3c572b5d621717a3e0e8a1ac081802c79cf
Midelio <afowl2087@gmail.com>
2026-03-11 11:22:17 +0100

    lame

--- P10/index.php
+++ P10/index.php
@@ -3,4 +3,7 @@
 $pdo = new PDO("mysql:host=localhost;dbname=school", "root", "", [
 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
+$stmt = $pdo->query("SELECT * FROM firstname " and "SELECT *FROM lastname ");
+$nom = $stmt->fetchAll();
+print_r($nom)
 ?>
