806a9c20bcc4b4743080922b22cf01bfca6cd987
Midelio <afowl2087@gmail.com>
2026-03-11 17:14:18 +0100

    Fin

--- P10/index.php
+++ P10/index.php
@@ -1,9 +1,25 @@
 <?php
 
+
 $pdo = new PDO("mysql:host=localhost;dbname=school", "root", "", [
 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
-$stmt = $pdo->query("SELECT * FROM firstname " and "SELECT *FROM lastname ");
+$sql = "SELECT `id`, `firstname`, `lastname`, `email` FROM `students` ORDER by `lastname`, `firstname` ASC;";
+$stmt = $pdo->query($sql);
 $nom = $stmt->fetchAll();
-print_r($nom);
+var_dump($nom)
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
